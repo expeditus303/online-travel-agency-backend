3a8cbb874d4788aef716f7aaf22caa0e006e23d7
expeditus303 <ricardotonietto@gmail.com>
2023-05-24 17:09:09 -0300

    feat: database connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors"
 import "dotenv/config"
+import db from "./config/database.connection.js";
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+db.query('SELECT * FROM flights')
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
---
+++ src/config/database.connection.js
@@ -0,0 +1,21 @@
+import pg from "pg"
+import "dotenv/config"
+
+const { Pool } = pg
+
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL
+}
+
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+const db = new Pool(configDatabase)
+
+db.query('SELECT NOW()', (err, res) => {
+    if(err) console.log(err)
+    console.log(res.rows[0])
+    db.end()
+})
+
+export default db
+
