10b26abf536d9a586cd854197ae341a31da154d7
expeditus303 <ricardotonietto@gmail.com>
2023-05-24 16:46:38 -0300

    feat: server config

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors"
+import "dotenv/config"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
